a5d9ef3eeab00ebca828b693d95f0d4bf7c1df15
Andre Zilhao <aalmsantos@gmail.com>
2019-01-19 02:32:28 +0000

    Added new scripts for floodfill algorithm

---
+++ Proj-4/ml-agents-master/UnitySDK/Assets/NodeComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeComponent : MonoBehaviour {
+
+    public List<NodeComponent> adjacencies;
+    public float value;
+    public bool Goal;
+
+    private void Update()
+    {
+        Color c = GetComponent<Renderer>().material.color;
+        c.r = 1-value;
+        c.g = value;
+        GetComponent<Renderer>().material.color = c;
+    }
+
+    public void UpdateValue(float val)
+    {
+        if (val > value)
+            value = val;
+    }
+}
