8980a8d9e35ac0e52b33a47d69cfc741e2142375
Andre Zilhao <aalmsantos@gmail.com>
2019-01-19 18:17:50 +0000

    Fixed Material Memory Leak in nodeComponent color changing scheme

--- Proj-4/ml-agents-master/UnitySDK/Assets/NodeComponent.cs
+++ Proj-4/ml-agents-master/UnitySDK/Assets/NodeComponent.cs
@@ -7,13 +7,26 @@ public class NodeComponent : MonoBehaviour {
     public List<NodeComponent> adjacencies;
     public float value;
     public bool Goal;
+    Material m;
+    Color c;
+
+    private void Start()
+    {
+        m = GetComponent<Renderer>().material;
+        c = m.color;
+    }
 
     private void Update()
     {
-        Color c = GetComponent<Renderer>().material.color;
         c.r = 1-value;
         c.g = value;
-        GetComponent<Renderer>().material.color = c;
+        m.color = c;
+        GetComponent<Renderer>().material = m;
+    }
+
+    public void OnDestroy()
+    {
+        Destroy(m);
     }
 
     public void UpdateValue(float val)
