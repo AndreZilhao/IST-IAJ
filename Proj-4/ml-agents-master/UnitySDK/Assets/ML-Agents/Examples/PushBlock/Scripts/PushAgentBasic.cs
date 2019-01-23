c2a345dd524569feed1bf542b638cf5ff103a3c8
Andre Zilhao <aalmsantos@gmail.com>
2019-01-23 23:05:45 +0000

    Merger again

--- Proj-4/ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/PushBlock/Scripts/PushAgentBasic.cs
+++ Proj-4/ml-agents-master/UnitySDK/Assets/ML-Agents/Examples/PushBlock/Scripts/PushAgentBasic.cs
@@ -95,6 +95,7 @@ public class PushAgentBasic : Agent
 
     public override void CollectObservations()
     {
+        float[] f = CollectRewardObs();
         /*
         foreach(NodeComponent n in visibleNodes)
         {
